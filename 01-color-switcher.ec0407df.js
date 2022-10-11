402846b2e0cd63852f42623a47241eee936e42eb
Vadym-Khokhlov <75025818+Vadym-Khokhlov@users.noreply.github.com>
2022-10-11 20:37:40 +0000

    Deploying to gh-pages from @ Vadym-Khokhlov/goit-js-hw-09@d640e88d7d2e120c7cfcdd96e0074e5ce846bd3c 🚀

--- 01-color-switcher.ec0407df.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;t.addEventListener("click",(function(){o=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));n.style.backgroundColor=t}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1,e.disabled=!0}))}();
-//# sourceMappingURL=01-color-switcher.ec0407df.js.map
